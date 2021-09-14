8545334870b9453813dba781ad3248f780b051e4
Andreea-c30 <c.1andreea874@gmail.com>
2021-09-14 19:25:56 +0300

    Updated the project with dependancy-injection

--- app/Http/Controllers/ContactUSController.php
+++ app/Http/Controllers/ContactUSController.php
@@ -3,45 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactUsRequest;
-use Illuminate\Contracts\Mail\Mailer;
+use App\Services\Mail\ContactUsMailer;
 use Illuminate\Http\Request;
-use Illuminate\Mail\Message;
-use Rules;
-use Mail;
+//use Illuminate\Mail\Message;
+
 class ContactUSController extends Controller
 {
     public function contacts(Request $request)
     {
-       //dd($request->all());
         
        return view('pages/contacts');
     }   
 
-    public function storeContactInfo(ContactUsRequest $request, Mailer $mailer)
+    public function storeContactInfo(ContactUsRequest $request, ContactUsMailer $mailer)
     {
       $data = $request -> validated();
-      $data['messageText']=$data['message'];
-      $callback=function($input='')
-      {
-        return $input . 'text';
-      };
-     
-      //dd($request -> validated());
-  
-    //\Log::debug($callback(),$request->all());
-  
-   //\Log::info($callback('Validated'),$request->validated());
+      $mailer->send($data);
 
-   $mailer->send(
-     'emails/contactUs',
-     $data,
-     function(Message $message)use($data){
-$message->to('test@test.com');
-$message->subject('Contact us request form '. $data['name'].' '.$data['email']);
-$message->replyTo($data['email']);
-     }
-   );
-      return redirect(route('contactUs.show'));
+      return redirect(route('contactUs.show'))->withInput($data);
 
     }    
 }
